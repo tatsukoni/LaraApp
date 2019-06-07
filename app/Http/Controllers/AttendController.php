c025fac6cc44d476c948a8a8a71e77c3337b45dd
tatsukoni <tatsukoni1818ezweb.ne.jp>
2019-06-07 13:26:41 +0900

    モデルに複数のprimaryKeyを指定し、出欠の更新挙動を修正

--- app/Http/Controllers/AttendController.php
+++ app/Http/Controllers/AttendController.php
@@ -74,10 +74,13 @@ class AttendController extends Controller
         $attends = Attend::where('userId', $userId)
                 ->where('scheduleId', $scheduleId)
                 ->get();
+        //dd($attends);
         foreach($attends as $attend) {
+            dump($attend);
             $attend->update(['attend' => $request->$count]);
             $count += 1;
         }
+        //dd();
         return redirect()->action(
             'ScheduleController@show', ['scheduleId' => $scheduleId]
         );
